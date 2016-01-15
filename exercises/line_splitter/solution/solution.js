e78d7eb185e534b6ca77e25c300a693d121f035f
Dave Kerr <davek09@gmail.com>
2016-01-14 20:24:42 -0500

    update line-splitter solution to use a readStream

--- exercises/line_splitter/solution/solution.js
+++ exercises/line_splitter/solution/solution.js
@@ -1,12 +1,15 @@
-var fs = require('fs')
-var file = fs.readFileSync(process.argv[2])
-var offset = 0, NL = '\n'.charCodeAt(0)
+var fs = require('fs');
+var offset = 0, NL = '\n'.charCodeAt(0);
 
-for (var i = 0; i < file.length; i++) {
-  if (file[i] === NL) {
-    console.log(file.slice(offset, i))
-    i++
-    offset = i
+function splitNewLine(file) {
+  for (var i = 0; i < file.length; i++) {
+    if (file[i] === NL) {
+      console.log(file.slice(offset, i));
+      offset = i + 1;
+    }
   }
+  console.log(file.slice(offset, i));
 }
-console.log(file.slice(offset, i))
+
+fs.createReadStream(process.argv[2]).on('data', splitNewLine);
+
